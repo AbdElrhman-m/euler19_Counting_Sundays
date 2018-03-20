3292e45df0f6584c6cc5f217fd5031674efc256c
AbdElrhman <37018521+superbido@users.noreply.github.com>
2018-03-20 12:08:35 +0200

    Create sol_Counting_Sundays.py

---
+++ sol_Counting_Sundays.py
@@ -0,0 +1,62 @@
+import datetime
+def isLeapYear(year):
+    if year % 400 == 0:
+        return True
+    if year % 100 == 0:
+        return False
+    if year % 4 == 0:
+        return True
+    return False    
+def daysInMonth(year, month):
+    if month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12:
+        return 31
+    if month == 2:
+        if isLeapYear(year):
+            return 29 
+        else:
+            return 28       
+    else:   
+        return 30
+def nextDay(year, month, day):
+    """Simple version: assume every month has 30 days"""
+    if day < daysInMonth(year, month):
+        return year, month, day + 1
+    else:
+        if month == 12:
+            return year + 1, 1, 1
+        else:
+            return year, month + 1, 1
+        
+def dateIsBefore(year1, month1, day1, year2, month2, day2):
+    """Returns True if year1-month1-day1 is before year2-month2-day2. Otherwise, returns False."""
+    if year1 < year2:
+        return True
+    if year1 == year2:
+        if month1 < month2:
+            return True
+        if month1 == month2:
+            return day1 < day2
+    return False        
+def is_sunday(year, month, day):
+    ans = datetime.date(year, month, day)
+    day_name = ans.strftime("%a")
+    if day_name == "Sun":
+        return True
+    return False
+
+def sun_days_calc(year1, month1, day1, year2, month2, day2):
+    """how many sundays between dates."""
+    # program defensively! Add an assertion if the input is not valid!
+    assert not dateIsBefore(year2, month2, day2, year1, month1, day1)
+    days = 0
+    while dateIsBefore(year1, month1, day1, year2, month2, day2):
+        year1, month1, day1 = nextDay(year1, month1, day1)
+        if  is_sunday(year1, month1, day1) == True and day1 == 1a: 
+            days += 1
+    return days
+
+
+
+print sun_days_calc(1901,1,1,2000,12,31)
+
+
